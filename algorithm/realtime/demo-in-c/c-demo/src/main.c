f5a8c5e3a7d6112ba7e06cd1e1615d841cb13dd3
Jerry Qian <jerry@qian.ninja>
2018-10-13 15:16:59 -0400

    c demo starter project files

---
+++ algorithm/realtime/demo-in-c/c-demo/src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TestProject3.c
+ Author      : Jerry Qian
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello Hyperloop!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
