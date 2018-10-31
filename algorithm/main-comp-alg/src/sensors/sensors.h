01a512784e846ba162b84f3d89fc0a126b582405
Mark <markbthr@gmail.com>
2018-10-30 21:13:45 -0400

    Restructured file heirarchy

---
+++ algorithm/main-comp-alg/src/sensors/sensors.h
@@ -0,0 +1,4 @@
+#ifndef __SENSORS__
+/*This is where our includes of other sensor files will live*/
+#define __SENSORS__
+#endif
---
+++ algorithm/main-comp-alg/src/states/states.h
@@ -0,0 +1,4 @@
+#ifndef __STATES__
+/* This is where our state includes will live */
+#define __STATES__
+#endif
