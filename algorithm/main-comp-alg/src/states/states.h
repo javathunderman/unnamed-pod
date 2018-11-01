4327235de337529237c1fab51d9d81745c659257
Jerry Qian <jerry@qian.ninja>
2018-11-01 19:06:29 -0400

    Update states.h

--- algorithm/main-comp-alg/src/states/states.h
+++ algorithm/main-comp-alg/src/states/states.h
@@ -1,5 +1,11 @@
 #ifndef __STATES__
 /* This is where our state includes will live */
+#include "standby.c"
+#include "service.c"
+#include "initialize.c"
+#include "accelerate.c"
+#include "normbrake.c"
+#include "estop.c"
 
 #define __STATES__
 #endif
