1f0453525c0b10712d491fd45b3c5f49b615954f
Jerry Qian <jerry@qian.ninja>
2018-11-20 19:12:52 -0500

    pthread work/testing

--- algorithm/main-comp-alg/src/main_alg.c
+++ algorithm/main-comp-alg/src/main_alg.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
-#include "./sensors/sensors.h"
-#include "./states/states.h"
+#include <stdlib.h> 
+#include <unistd.h>
+#include <pthread.h> 
+/*#include "./sensors/sensors.h"
+#include "./states/states.h"*/
 
 int testfun(int i) {
 	printf("Printing integer: %d\n", i);
+	return 0;
+}
+
+void *threadfun(void * i) {
+	int *ii = (int *)i;
+	printf("Printing integer: %d\n", *ii);
+	return NULL;
 }
 
 int main() {
-    int (*fp_arr[6][6]) (int x);
-	//void (*fun_ptr)(int) = &testfun;
+    void * (*fp_arr[6][6]) (void *);
+	void (*testfun_ptr)(int) = &testfun;
+	void * (*threadfun_ptr)(void *) = &threadfun;
 
-    fp_arr[1][1] = &testfun;
-	fp_arr[1][2] = &estoptestfun;
-    
-	(*fp_arr[1][1])(10);
-	(*fp_arr[1][2])(5);
+	pthread_t thread_id;
+	int *param = (int *)malloc(2*sizeof(int));
+
+    fp_arr[1][1] = &threadfun;
+
+	param[0] = 100;
+	param[1] = 200;
+    pthread_create(&thread_id, NULL, fp_arr[1][1], param); 
+    pthread_join(thread_id, NULL); 
+    free(param);
 
 	return 0;
 }
