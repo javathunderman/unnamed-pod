33522c83b6f8b9459205bf0fd2e1e9278e102ff5
Jerry Qian <jerry@qian.ninja>
2018-11-30 16:08:23 -0500

    config yaml loading test

---
+++ algorithm/main-comp-alg/src/config_parser.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <yaml.h>
+
+int main() {
+	
+
+
+	
+
+	return 0;
+}
--- algorithm/main-comp-alg/src/main_alg.c
+++ algorithm/main-comp-alg/src/main_alg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <unistd.h>
+#include <yaml.h>
 #include "./sensors/sensors.h"
 #include "./states/states.h"
 
@@ -17,8 +18,55 @@ int service_state();
 int accelerate_state();
 int normbrake_state();
 int estop_state();
-
+//TO DO: Thresholds from config file. 
 int main() {
+	FILE *fh = fopen("config.yaml", "r");
+	yaml_parser_t parser;
+	yaml_token_t  token;
+
+	/* Initialize parser */
+	if(!yaml_parser_initialize(&parser))
+	fputs("Failed to initialize parser!\n", stderr);
+	if(fh == NULL)
+	fputs("Failed to open file!\n", stderr);
+
+	/* Set input file */
+	yaml_parser_set_input_file(&parser, fh);
+
+	/* CODE HERE */
+	do {
+		yaml_parser_scan(&parser, &token);
+		switch(token.type) {
+			/* Stream start/end */
+			case YAML_STREAM_START_TOKEN: puts("STREAM START"); break;
+			case YAML_STREAM_END_TOKEN:   puts("STREAM END");   break;
+			/* Token types (read before actual token) */
+			case YAML_KEY_TOKEN:   printf("(Key token)   "); break;
+			case YAML_VALUE_TOKEN: printf("(Value token) "); break;
+			/* Block delimeters */
+			case YAML_BLOCK_SEQUENCE_START_TOKEN: puts("<b>Start Block (Sequence)</b>"); break;
+			case YAML_BLOCK_ENTRY_TOKEN:          puts("<b>Start Block (Entry)</b>");    break;
+			case YAML_BLOCK_END_TOKEN:            puts("<b>End block</b>");              break;
+			/* Data */
+			case YAML_BLOCK_MAPPING_START_TOKEN:  puts("[Block mapping]");            break;
+			case YAML_SCALAR_TOKEN:  printf("scalar %s \n", token.data.scalar.value); break;
+			/* Others */
+			default:
+				printf("Got token of type %d\n", token.type);
+		}
+		if(token.type != YAML_STREAM_END_TOKEN)
+			yaml_token_delete(&token);
+		} while(token.type != YAML_STREAM_END_TOKEN);
+		yaml_token_delete(&token);
+	/* END new code */
+
+	/* Cleanup */
+	yaml_parser_delete(&parser);
+	fclose(fh);
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	/* REST OF CODE STARTS HERE */
+	//////////////////////////////////////////////////////////////////////////////////////////////////
 	int sid_arr[NUM_STATES][NUM_CODES];
 	int (*fp_arr[NUM_STATES]) (void);
 
