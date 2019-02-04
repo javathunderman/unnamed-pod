93a905eaa3f026914310e870b3d20ea3d91cffd7
Jerry Qian <jerry@qian.ninja>
2019-02-04 17:38:33 -0500

    typo

--- algorithm/main-comp-alg/src/states/states.h
+++ algorithm/main-comp-alg/src/states/states.h
@@ -1,4 +1,4 @@
-p#ifndef __STATES__
+#ifndef __STATES__
 /* This is where our state includes will live */
 typedef enum {STANDBY_SID = 0, INITIALIZE_SID = 1, SERVICE_SID = 2, ACCELERATE_SID = 3, NORMBRAKE_SID = 4, ESTOP_SID = 5, NUM_STATES} State; 
 typedef enum {SUCCESS = 0, REPEAT = 1, ERROR = 2, ESTOP = 3, NUM_CODES} State_Status; 
