99aed2915993d9dc9260f3a32d6296140e025868
rfwheel <43093238+rfwheel@users.noreply.github.com>
2019-05-23 21:07:23 -0400

    Refactored code generation to run as thread

--- Telemetry_Framework/Python/CodeSource.py
+++ Telemetry_Framework/Python/CodeSource.py
@@ -65,8 +65,13 @@ def defines(tlm):
 
 def main_thread(tlm):
     # Begin writing send_tlm function
-    main = 'void send_tlm(int socket, SA * dest_addr, socklen_t dest_len) {\n'
+    main = 'void *send_tlm(void *args) {\n'
     main += '    Telemetry tlm;\n\n'
+    
+    # Unpack thread arguments
+    main += '    int socket = ((TelemetryArgs *)args)->socket;\n'
+    main += '    SA * dest_addr = ((TelemetryArgs *)args)->dest_addr;\n'
+    main += '    socklen_t dest_len = ((TelemetryArgs *)args)->dest_len;\n\n'
     
     main += current_time()
     
