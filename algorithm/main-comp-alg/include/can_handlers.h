a72510df71a12b525f1beddb5d740f2716f378ed
rfwheel <43093238+rfwheel@users.noreply.github.com>
2019-06-16 16:09:55 -0400

    Started making progress on CAN stuff.

--- algorithm/main-comp-alg/include/can_handlers.h
+++ algorithm/main-comp-alg/include/can_handlers.h
@@ -2,19 +2,7 @@
 #define CAN_HANDLERS_H
 
 #include "vs_can_api.h"
-
-/* This struct holds data received from CAN devices to be used
- * by the state machine.
- * 
- * IMPORTANT: Every load/store using this struct must use
- * gcc atomics to guarantee correctness of simultaneous
- * thread access.
- */
-typedef struct {
-    volatile short pack_soc;               /* Percent         */
-    volatile short pack_voltage;           /* deci-Volts      */
-    volatile short pack_current;           /* deci-Amps       */
-} CAN_Data;
+#include "can_master.h"
 
 
 /* CAN Receive Handler Functions */
@@ -25,11 +13,13 @@ void iso_state_handler(VSCAN_MSG *msg, CAN_Data *data);
 void iso_resistance_handler(VSCAN_MSG *msg, CAN_Data *data);
 void iso_error_handler(VSCAN_MSG *msg, CAN_Data *data);
 void lipo_handler(VSCAN_MSG *msg, CAN_Data *data);
+void ready_to_transmit_handler(VSCAN_MSG *msg, CAN_Data *data);
+void transmit_enable_handler(VSCAN_MSG *msg, CAN_Data *data);
 void max_speed_handler(VSCAN_MSG *msg, CAN_Data *data);
 void device_current_handler(VSCAN_MSG *msg, CAN_Data *data);
 void current_200pc_handler(VSCAN_MSG *msg, CAN_Data *data);
-void true_speed_handler(VSCAN_MSG *msg, CAN_Data *data);
-void true_current_handler(VSCAN_MSG *msg, CAN_Data *data);
-void true_position_handler(VSCAN_MSG *msg, CAN_Data *data);
+void actual_speed_handler(VSCAN_MSG *msg, CAN_Data *data);
+void actual_current_handler(VSCAN_MSG *msg, CAN_Data *data);
+void actual_position_handler(VSCAN_MSG *msg, CAN_Data *data);
 
 #endif
