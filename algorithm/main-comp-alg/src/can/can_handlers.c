2148d4ec6ad141427921837b7a906f2c7f6b0a73
mangomadhava <42628029+mangomadhava@users.noreply.github.com>
2019-06-17 22:14:27 -0400

    Added a few functions

--- algorithm/main-comp-alg/src/can/can_handlers.c
+++ algorithm/main-comp-alg/src/can/can_handlers.c
@@ -23,3 +23,78 @@ void bms_pack_handler(VSCAN_MSG *msg, CAN_Data *data) {
     __atomic_store_n(&(data->pack_voltage), pack_voltage, __ATOMIC_RELAXED);
     __atomic_store_n(&(data->pack_current), pack_current, __ATOMIC_RELAXED);
 }
+/* This function updates state machine's CAN_Data with:
+ *     Pack Minimum voltage
+ *     Pack Maximum Voltage
+ *     Pack Average Temperature
+ *     Pack Highest Temperature
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void bms_other_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short min_vol = *((short *) (&(msg->Data[0])));
+	short max_vol = *((short *) (&(msg->Data[2])));
+	short avg_temp = *((short *) (&(msg->Data[4])));
+	short high_temp = *((short *) (&(msg->Data[6])));
+
+	__atomic_store_n(&(data->min_vol), min_vol, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->max_vol), max_vol, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->avg_temp), avg_temp, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->high_temp), high_temp, __ATOMIC_RELAXED);
+
+}
+
+/* This function updates state machine's CAN_Data with:
+ *     Pack Failsafe Status
+ *     Pack DTC Flags #1
+ *     Pack DTC Flags #2
+ *     Pack Rolling Counter
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+
+void bms_error_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short failsafe_status = *((short *) (&(msg->Data[0])));
+	short dtc_flags_1 = *((short *) (&(msg->Data[1])));
+	short dtc_flags_2 = *((short *) (&(msg->Data[2])));
+	short rolling_counter = *((short *) (&(msg->Data[4])));
+
+	__atomic_store_n(&(data->failsafe_status), failsafe_status, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->dtc_flags_1), dtc_flags_1, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->dtc_flags_2), dtc_flags_2, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->rolling_counter), rolling_counter, __ATOMIC_RELAXED);
+
+}
+/*NOT FINISHED*/
+/* This function updates state machine's CAN_Data with:
+ *     Pack Status Flags
+ *     Pack Electrical Isolation (ohm/vol)
+ *     
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void iso_state_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short status_flags = *((short *) (&(msg->Data[0])));
+	short electrical_isolation = *((short *) (&(msg->Data[1])));
+	short uncertainty_1 = *((short *) (&(msg->Data[3])));
+	short energy_stored = *((short *) (&(msg->Data[5])));
+	short uncertainty_2 = *((short *) (&(msg->Data[7])));
+
+
+}
+
