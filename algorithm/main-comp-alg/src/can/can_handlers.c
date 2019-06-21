cfc074400eff1edb51334e4f8cdd6d0b3244da87
mangomadhava <42628029+mangomadhava@users.noreply.github.com>
2019-06-21 17:10:45 -0400

    Added handler functions

--- algorithm/main-comp-alg/src/can/can_handlers.c
+++ algorithm/main-comp-alg/src/can/can_handlers.c
@@ -24,11 +24,13 @@ void bms_pack_handler(VSCAN_MSG *msg, CAN_Data *data) {
     __atomic_store_n(&(data->pack_voltage), pack_voltage, __ATOMIC_RELAXED);
     __atomic_store_n(&(data->pack_current), pack_current, __ATOMIC_RELAXED);
 }
+
+
 /* This function updates state machine's CAN_Data with:
- *     Pack Minimum voltage
- *     Pack Maximum Voltage
- *     Pack Average Temperature
- *     Pack Highest Temperature
+ *     Minimum voltage
+ *     Maximum Voltage
+ *     Average Temperature
+ *     Highest Temperature
  *
  * Params:
  *     VSCAN_MSG *msg -> pointer to received CAN message frame
@@ -38,23 +40,25 @@ void bms_pack_handler(VSCAN_MSG *msg, CAN_Data *data) {
  *     void
  */
 void bms_other_handler(VSCAN_MSG *msg, CAN_Data *data){
-	short min_vol = *((short *) (&(msg->Data[0])));
-	short max_vol = *((short *) (&(msg->Data[2])));
+    short min_voltage = *((short *) (&(msg->Data[0])));
+	short max_voltage = *((short *) (&(msg->Data[2])));
 	short avg_temp = *((short *) (&(msg->Data[4])));
 	short high_temp = *((short *) (&(msg->Data[6])));
 
-	__atomic_store_n(&(data->min_vol), min_vol, __ATOMIC_RELAXED);
-	__atomic_store_n(&(data->max_vol), max_vol, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->min_voltage), min_voltage, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->max_voltage), max_voltage, __ATOMIC_RELAXED);
 	__atomic_store_n(&(data->avg_temp), avg_temp, __ATOMIC_RELAXED);
 	__atomic_store_n(&(data->high_temp), high_temp, __ATOMIC_RELAXED);
 
 }
 
+
+
 /* This function updates state machine's CAN_Data with:
- *     Pack Failsafe Status
- *     Pack DTC Flags #1
- *     Pack DTC Flags #2
- *     Pack Rolling Counter
+ *     Failsafe Status
+ *     DTC Flags #1
+ *     DTC Flags #2
+ *     Rolling Counter
  *
  * Params:
  *     VSCAN_MSG *msg -> pointer to received CAN message frame
@@ -63,7 +67,6 @@ void bms_other_handler(VSCAN_MSG *msg, CAN_Data *data){
  * Returns:
  *     void
  */
-
 void bms_error_handler(VSCAN_MSG *msg, CAN_Data *data){
 	short failsafe_status = *((short *) (&(msg->Data[0])));
 	short dtc_flags_1 = *((short *) (&(msg->Data[1])));
@@ -76,11 +79,16 @@ void bms_error_handler(VSCAN_MSG *msg, CAN_Data *data){
 	__atomic_store_n(&(data->rolling_counter), rolling_counter, __ATOMIC_RELAXED);
 
 }
-/*NOT FINISHED*/
+
+
+
+
 /* This function updates state machine's CAN_Data with:
- *     Pack Status Flags
- *     Pack Electrical Isolation (ohm/vol)
- *     
+ *     Status Flags
+ *     Electrical Isolation (ohm/vol)
+ *     Electrical Isolation Uncertainty
+ *     Energy Stored
+ *     Energy Stored Uncertainty
  *
  * Params:
  *     VSCAN_MSG *msg -> pointer to received CAN message frame
@@ -90,12 +98,216 @@ void bms_error_handler(VSCAN_MSG *msg, CAN_Data *data){
  *     void
  */
 void iso_state_handler(VSCAN_MSG *msg, CAN_Data *data){
-	short status_flags = *((short *) (&(msg->Data[0])));
-	short electrical_isolation = *((short *) (&(msg->Data[1])));
-	short uncertainty_1 = *((short *) (&(msg->Data[3])));
+	short status_flags = *((short *) (&(msg->Data[1])));
+	short electrical_isolation = *((short *) (&(msg->Data[2])));
+	short electrical_isolation_uncert = *((short *) (&(msg->Data[4])));
 	short energy_stored = *((short *) (&(msg->Data[5])));
-	short uncertainty_2 = *((short *) (&(msg->Data[7])));
+	short energy_stored_uncert = *((short *) (&(msg->Data[7])));
 
+	__atomic_store_n(&(data->status_flags), status_flags, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->electrical_isolation), electrical_isolation, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->electrical_isolation_uncert), electrical_isolation_uncert, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->energy_stored), energy_stored, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->energy_stored_uncert), energy_stored_uncert, __ATOMIC_RELAXED);
 
 }
 
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     status flags
+ *     rp Isolation resistance
+ *     rp Isolation resistance uncertainty
+ *     rn Isolation resistance
+ *     rn Isolation resistance uncertainty
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void iso_resistance_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short status_flags = *((short *) (&(msg->Data[1])));
+	short rp_iso_resistance = *((short *) (&(msg->Data[2])));
+	short rp_iso_resistance_uncert = *((short *) (&(msg->Data[4])));
+	short rn_iso_resistance = *((short *) (&(msg->Data[5])));
+	short rn_iso_resistance_uncert = *((short *) (&(msg->Data[7])));
+
+	__atomic_store_n(&(data->status_flags), status_flags, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->rp_iso_resistance), rp_iso_resistance, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->rp_iso_resistance_uncert), rp_iso_resistance_uncert, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->rn_iso_resistance), rn_iso_resistance , __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->rn_iso_resistance_uncert), rn_iso_resistance_uncert, __ATOMIC_RELAXED);
+}
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Pack Status Flags
+ *     Pack error flags
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void iso_error_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short status_flags = *((short *) (&(msg->Data[1])));
+	short error_flags = *((short *) (&(msg->Data[2])));
+
+	__atomic_store_n(&(data->status_flags), status_flags, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->error_flags), error_flags, __ATOMIC_RELAXED);
+
+}
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Pack status flags
+ *     Pack battery voltage
+ *     Pack battery voltage uncertainty
+ *     Pack Max battery voltage
+ *     Pack max battery voltage uncertainty
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void lipo_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short status_flags = *((short *) (&(msg->Data[1])));
+	short battery_volt = *((short *) (&(msg->Data[2])));
+	short battery_volt_uncert = *((short *) (&(msg->Data[4])));
+	short max_battery_volt = *((short *) (&(msg->Data[5])));
+	short max_battery_volt_uncert = *((short *) (&(msg->Data[7])));
+
+	__atomic_store_n(&(data->status_flags), status_flags, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->battery_volt), battery_volt, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->battery_volt_uncert), battery_volt_uncert, __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->max_battery_volt), max_battery_volt , __ATOMIC_RELAXED);
+	__atomic_store_n(&(data->max_battery_volt_uncert), max_battery_volt_uncert, __ATOMIC_RELAXED);
+}
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Pack max speed
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void max_speed_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short max_speed = *((short *) (&(msg->Data[1])));
+	__atomic_store_n(&(data->max_speed), max_speed, __ATOMIC_RELAXED);
+
+}
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Pack device current
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void device_current_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short dev_current = *((short *) (&(msg->Data[1])));
+	__atomic_store_n(&(data->dev_current), dev_current, __ATOMIC_RELAXED);
+}
+
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Current 200pc
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void current_200pc_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short current_200pc = *((short *) (&(msg->Data[1])));
+	__atomic_store_n(&(data->current_200pc), current_200pc, __ATOMIC_RELAXED);
+
+}
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Percent Max Spered
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void true_speed_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short percent_max_speed = *((short *) (&(msg->Data[1])));
+	__atomic_store_n(&(data->percent_max_speed), percent_max_speed, __ATOMIC_RELAXED);
+
+}
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     True current
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void true_current_handler(VSCAN_MSG *msg, CAN_Data *data){
+	short true_current = *((short *) (&(msg->Data[1])));
+	__atomic_store_n(&(data->true_current), true_current, __ATOMIC_RELAXED);
+}
+
+
+
+
+
+/* This function updates state machine's CAN_Data with:
+ *     Revolutions
+ *
+ * Params:
+ *     VSCAN_MSG *msg -> pointer to received CAN message frame
+ *     CAN_Data *data -> pointer to CAN_data struct used by state machine
+ *
+ * Returns:
+ *     void
+ */
+void true_position_handler(VSCAN_MSG *msg, CAN_Data *data){
+	int revolutions = *((int *) (&(msg->Data[1])));
+	__atomic_store_n(&(data->revolutions), revolutions, __ATOMIC_RELAXED);
+
+
+}
